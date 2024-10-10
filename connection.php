7c7f414de1a69168f538ff79ac729f9e8b9ea2d1
DhruvaMakwana24 <makwanadhruva712@gmail.com>
2024-10-10 22:02:08 +0530

    added the files,folder

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+    $servername = "localhost";
+    $username = "u714635531_mavericks";
+    $password = "Mavericks_123";
+    $dbname = "u714635531_arcane";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
